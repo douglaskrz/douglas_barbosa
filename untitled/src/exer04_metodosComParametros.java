42ac6b7bbe79eefa73b01488c301efe6f92fae7d
Douglas Barbosa <douglasbarbosa.db10@gmail.com>
2026-08-18 21:02:45 -0300

    exer04

---
+++ untitled/src/exer04_metodosComParametros.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class exer04_metodosComParametros {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       int[] numeros = new int[5];
+
+       for(int i=0; i<5; i++){
+           System.out.println("Informe os valores: ");
+           numeros[i] = sc.nextInt();
+       }
+
+        sc.close();
+    }
+
+    public static void maiorElemento (){
+
+
+
+    }
+    public static void menorElemento (){
+
+
+
+    }
+    public static void mediaDosValores(int num){
+
+        for(int i=0; i<5; i++){
+
+        }
+
+
+    }
+
+
+
+
+
+}
