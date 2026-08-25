9dd0909f56740c40eb46c9ac37859a050b168ca1
Douglas Barbosa <douglasbarbosa.db10@gmail.com>
2026-08-25 19:10:20 -0300

    exer08

---
+++ untitled/src/exer_08metodosComParametros.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class exer_08metodosComParametros {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe a primeira nota: ");
+        double nota1 = sc.nextDouble();
+
+        System.out.println("Informe a segunda nota: ");
+        double nota2 = sc.nextDouble();
+        System.out.println("Informe a terceira nota: ");
+        double nota3 = sc.nextDouble();
+
+
+        double media = mediaAritmetica(nota1, nota2, nota3);
+        System.out.println("A média é: "+media);
+
+
+        sc.close();
+    }
+    public static double mediaAritmetica(double notaUM, double notaDOIS, double notaTRES){
+
+        return (notaUM + notaDOIS + notaTRES)/3;
+
+    }
+
+}
