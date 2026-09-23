bba45d69fc13fa9ecbec1a927ff655ba8d70d659
Douglas Barbosa <douglasbarbosa.db10@gmail.com>
2026-09-22 21:13:36 -0300

    .

--- poo_/src/exer08_poo.java
+++ poo_/src/exer08_poo.java
@@ -1,11 +1,11 @@
 public class exer08_poo {
     public static void main(String[] args) {
 
-        contribuintes pessoa1 = new contribuintes("João da Silva", "111.111.111.11", "RS", 3500, 0);
-        contribuintes pessoa2 = new contribuintes("Douglas", "222.222.222.22", "SC", 500000, 0);
-        contribuintes pessoa3 = new contribuintes("Matheus","333.333.333.33", "PR", 7000, 0);
-        contribuintes pessoa4 = new contribuintes("Maria", "444.444.444.44", "SP", 24000, 0);
-        contribuintes pessoa5 = new contribuintes("Luiza", "555.555.555.55", "ES", 34000, 0);
+        contribuintes pessoa1 = new contribuintes("João da Silva", "11111111111", "RS", 3500, 0);
+        contribuintes pessoa2 = new contribuintes("Douglas", "22222222222", "SC", 500000, 0);
+        contribuintes pessoa3 = new contribuintes("Matheus","33333333333", "PR", 7000, 0);
+        contribuintes pessoa4 = new contribuintes("Maria", "44444444444", "SP", 24000, 0);
+        contribuintes pessoa5 = new contribuintes("Luiza", "55555555555", "ES", 34000, 0);
 
        contribuintes[] contribuintes = { pessoa1, pessoa2, pessoa3, pessoa4, pessoa5};
 
